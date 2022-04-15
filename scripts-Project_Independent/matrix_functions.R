416cf1717402a3a60fb820ba5409c537e9e6f652
HumzaHemani <humzah@umich.edu>
2022-04-15 13:53:21 -0400

    Delete scripts-Project_Independent directory

--- scripts-Project_Independent/matrix_functions.R
+++
@@ -1,6 +0,0 @@
-
-R_removeAllZeroRows <- function(matrix) {
-	print('This function can be used with Classes: matrix and sparseMatrix')
-	matrix_trimmed <- matrix[which(rowSums(matrix) != 0),]
-	return(matrix_trimmed)
-}
--- scripts-Project_Independent/plotting_functions.R
+++
@@ -1,8 +0,0 @@
-
-R_plotExploratory <- function(exploratory_plot, location, name) {
-	print(paste('Plot was saved as an exploratory figure as ', name, sep = ''))
-	print(paste('location: ', './figures/exploratory_figures/',location,'/',name,'.png',sep=''))
-	png(filename=paste('./figures/exploratory_figures/',location,'/',name,'.png',sep=''), height = 1200, width = 1200, units='px')
-	exploratory_plot
-	dev.off()
-}
